0ce588e3c304aecea305a8d43017c23a8b1a02b4
Ros Yurchyk <ros_ur@ukr.net>
2019-07-24 16:59:31 +0300

    changed variable names, formatting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-module.exports = function styled(argument = null) {
-    return function (str, ...args) {
-        return function(last = {}) {
-            const first = argument === null ? '' : argument(last);
-            
-            if(args.length === 0) return `${str}`.replace(/\n|\r|\t/g, "");
-            
-            let result = `${first}`;
-            for(let i = 0; i < args.length; i++) {
-                result += `${str[i]}${args[i](last)};`;
+module.exports = function styled(extendedFunction = null) {
+    return function (stringValues, ...query) {
+        return function(queryValue = {}) {
+            let result = `${extendedFunction === null ? '' : extendedFunction(queryValue)}${stringValues[0]}`;
+            if(query.length) {
+                for(let i = 0; i < query.length; i++) {
+                    result += `${query[i](queryValue)}${stringValues[i+1]}`;
+                } 
             }
             return result.replace(/\n|\r|\t/g, "");
         }
