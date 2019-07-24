695341c0f2324a0937e0db14e911f93232ec956f
Ros Yurchyk <ros_ur@ukr.net>
2019-07-24 15:24:16 +0300

    styled working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function styled() {
-
+module.exports = function styled(argument = null) {
+    return function (str, ...args) {
+        return function(last = {}) {
+            const first = argument === null ? '' : argument(last);
+            
+            if(args.length === 0) return `${str}`.replace(/\n|\r|\t/g, "");
+            
+            let result = `${first}`;
+            for(let i = 0; i < args.length; i++) {
+                result += `${str[i]}${args[i](last)};`;
+            }
+            return result.replace(/\n|\r|\t/g, "");
+        }
+    }
 }
